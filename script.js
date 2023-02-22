404a65a8382d24811ed6e410726c75fe1c09afa6
dsardila <dsardila1809@hotmail.com>
2023-02-22 15:26:00 -0500

    Selecting elements and starting conditions

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 'use strict';
+// Selecting elements
+const score0El = document.querySelector('#score--0');
+const score1El = document.querySelector('#score--1');
+const diceEl = document.querySelector('.dice');
+
+// Starting conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
